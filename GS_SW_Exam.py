b0284f9c39018c69197ba0eb40c06a94877fa70f
Linyan Ge <linyan.ge@groundspeed.local>
2019-10-20 15:59:33 -0400

    checkpoint

--- GS_SW_Exam.py
+++ GS_SW_Exam.py
@@ -3,61 +3,120 @@ import random
 import string
 
 class FakeMotorData():
-    def __init__(self):
-        pass
+    def __init__(self,DataBase_Name):
+        self.DBNAME = DataBase_Name
+        self.conn = sqlite3.connect(self.DBNAME)
+        self.cur = self.conn.cursor()
     def create_fake_VIN(self):
         """ creat fake VIN number.
         :return: a rondom ID
         """
         vin_length = 17
-        random_char = string.ascii_uppercase + string.digits
+        random_char = string.ascii_uppercase + string.ascii_lowercase + string.digits
 
         return ''.join(random.choice(random_char) for _ in range(vin_length))
     def check_unique_ID(self):
         pass
-    def insert_data(self):
+    def insert_car_data(self,  list_of_columns):
         tuple_transformed = tuple(list_of_columns)
-        statement = '''insert into Countries Values (Null,?,?,?,?,?,?,?)'''
-        cur.execute(statement, tuple_transformed)
-    def get_data(self):
+        statement = '''insert into Motors Values (Null,?,?,?,?,?,?,?,?)'''
+        self.cur.execute(statement, tuple_transformed)
+
+    def insert_claims_data(self,list_of_columns):
+        tuple_transformed = tuple(list_of_columns)
+        statement = '''insert into Claims Values (Null,?,?,?,?,?,?,?,?,?)'''
+        self.cur.execute(statement, tuple_transformed)
+    def get_motors_data(self,list_of_columns):
+        tuple_transformed = tuple(list_of_columns)
+        statement = '''select * from Motors'''
+        '''select c.EnglishName, c.Region, count(b.SpecificBeanBarName) 
+                                                        from Countries as c
+                                                        join Bars as b on b.? = c.Id
+                                                        where c.Region = ?
+                                                        group by c.EnglishName
+                                                        having count(b.SpecificBeanBarName) >4
+                                                        order by count(b.SpecificBeanBarName)  desc'''
+        result_cn = self.cur.execute(statement, tuple_transformed)
+
+        self.conn.commit()
+        for row in result_cn:
+            print(row)
+    def get_claims_data(self):
         pass
 
 
 class MotorVehicleDB():
-    def __init__(self):
-        pass
-    def create_db(self,db_name,table_list):
-        DBNAME = '%s.db'%(db_name)
-        try :
+    def __init__(self,db_name):
+        self.DBNAME = '%s.db' % (db_name)
+        try:
 
-            conn = sqlite3.connect(DBNAME)
-            cur = conn.cursor()
-            print('successfully create database %s'%(db_name))
+            self.conn = sqlite3.connect(self.DBNAME)
+            self.cur = self.conn.cursor()
+            print('successfully create database %s' % (db_name))
         except:
             print('there\'s an error when creating an database')
-        for table_name in table_list:
-            statement = '''
-                    DROP TABLE IF EXISTS 'Bars';
-                '''
-            cur.execute(statement)
+    def create_table(self):
+
+        statement = '''
+                DROP TABLE IF EXISTS 'Motors';
+            '''
+        self.cur.execute(statement)
+        statement = '''
+                        DROP TABLE IF EXISTS 'Claims';
+                    '''
+        self.cur.execute(statement)
 
 
-            statement = '''create table 'Countries'(
+        statement = '''create table 'Motors'(
                             'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
-                            'Alpha2' TEXT ,
-                            'Alpha3' TEXT ,
-                            'EnglishName' TEXT ,
-                            'Region' TEXT ,
-                            'Subregion' TEXT,
-                            'Population' INTEGER ,
-                            'Area' REAL
+                            'Year' INTEGER ,
+                            'Make' TEXT ,
+                            'Model' TEXT ,
+                            'PurchaseValue' TEXT ,
+                            'VIN' TEXT,
+                            'PlateState' INTEGER ,
+                            'number' REAL
                             )
                             '''
 
-        cur.execute(statement)
-        conn.commit()
+        self.cur.execute(statement)
+        self.conn.commit()
+        statement = '''create table 'Claims'(
+                                    'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    'LOSSYEAR' INTEGER ,
+                                    'DESCRIPTION' TEXT ,
+                                    'TOTALPAID' REAL ,
+                                    'VIN' TEXT,
+                                    'LossState' INTEGER ,
+                                    'MotorID' INTERGER ,
+                                    'VINNUMBER' INTERGER NOT NULL,
+                                    'OTHERINFO' TEXT,
+                                    FOREIGN KEY('VINNUMBER') REFERENCES Motors(VIN)
+                                    )
+                                    '''
+
+        self.cur.execute(statement)
+        self.conn.commit()
 
     def get_summary(self):
+        '''select c.EnglishName, c.Region, count(b.SpecificBeanBarName)
+                                                from Countries as c
+                                                join Bars as b on b.? = c.Id
+                                                where c.Region = ?
+                                                group by c.EnglishName
+                                                having count(b.SpecificBeanBarName) >4
+                                                order by count(b.SpecificBeanBarName)  desc'''
         pass
 
+
+if __name__ == '__main__':
+
+    DataBase_Name = 'GS_MotorVehicle'
+    #create database
+    testdb = MotorVehicleDB(DataBase_Name)
+    testdb.create_table()
+    # create data
+
+    testcar = FakeMotorData(DataBase_Name)
+    testcar.insert_car_data(list_of_columns=("NULL",'1','2','3','4',testcar.create_fake_VIN(),'6','7'))
 
